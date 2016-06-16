fe816c159d6a7223076a8ced9d19ef1e5da4937f
Shreyams Jain <shreyams.jain@sjsu.edu>
2016-06-16 05:22:49 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yelp::Application.config.secret_key_base = 'ed398f930ecc6bf9efe5d802c96fab51847060f3cf02b26b764662b70194b6038f3569dae3726b0f9899d5273b515c5745406e3501b24907cc89ef09894f6d0d'
