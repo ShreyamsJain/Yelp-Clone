f772d3e1bd467e7c9049038a6c7d3f4f1b9ce8db
Shreyams Jain <shreyams.jain@sjsu.edu>
2016-06-16 09:09:45 +0000

    Create sample home page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Yelp::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  # Adding places controller
+  root 'places#index'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
