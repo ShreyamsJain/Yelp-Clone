3f0d85ba10b90dd1f275690f232a8bd8651e98fa
Shreyams Jain <shreyams.jain@sjsu.edu>
2016-07-04 18:12:27 +0000

    Adjust the automated email mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,10 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@yelpshreyams.com"
 
-  def comment_added
-  	mail(to: "shreyams.jain22@gmail.com",
-  		subject: "A comment has been added to your place")
+  def comment_added(comment)
+  	@place = comment.place
+  	@place_owner = comment.user
+  	mail(to: @place_owner.email,
+  		subject: "A comment has been added to your #{@place.name}")
   end
 end
